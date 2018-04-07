e6b48707c09d488315e041cafee1aa9082bef65f
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-04-07 18:14:48 -0300

    Create CoapService Class

---
+++ src/main/java/Client.java
@@ -0,0 +1,12 @@
+import org.eclipse.californium.core.CoapClient;
+import static org.eclipse.californium.core.coap.MediaTypeRegistry.APPLICATION_JSON;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String url = "coap://localhost:5683/sensor";
+        CoapClient coapClient = new CoapClient(url);
+        coapClient.post("{\"type\": \"ACC\", \"ts\":12412214231, \"user\":\"qw3q)io\", \"value\" : [1,2,3]}", APPLICATION_JSON);
+        System.exit(0);
+    }
+}
--- src/main/java/CoAPService.java
+++ src/main/java/CoAPService.java
@@ -1,3 +1,15 @@
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.EndpointManager;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
 /**
  * University of Sao Paulo
  * IoT Repository Module
@@ -5,13 +17,53 @@
  * @author Vinicius A. Barros
  * @author Sergio Baptista
  */
-public class CoAPService {
+public class CoAPService extends CoapServer {
 
-    public static void run(String[] args) {
+    private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
 
+    public static void run(String [] args){
+        try {
+            CoAPService server = new CoAPService();
+            server.addEndpoints();
+            server.start();
+        } catch (SocketException e) {
+            System.err.println("Failed to initialize server: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        run(args);
+       run(args);
+    }
+
+    private void addEndpoints() {
+        for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
+            if (addr instanceof Inet4Address || addr.isLoopbackAddress()) {
+                InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
+                addEndpoint(new CoapEndpoint(bindToAddress));
+            }
+        }
+    }
+
+    public CoAPService() throws SocketException {
+        add(new CoapServerResource());
+    }
+
+    class CoapServerResource extends CoapResource {
+
+        public CoapServerResource() {
+            super("sensor");
+        }
+
+        @Override
+        public void handleGET(CoapExchange exchange) {
+            exchange.respond("Hello World!");
+        }
+
+        @Override
+        public void handlePOST(CoapExchange exchange) {
+            String msg = exchange.getRequestText();
+            System.out.println(msg);
+            exchange.accept();
+        }
     }
 }
