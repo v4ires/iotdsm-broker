2d7211564e4dee9c9460aba8211b046bbe065200
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-04-10 09:57:50 -0300

    Fixed iotdsm-start.sh

--- iotdsm-start.sh
+++ iotdsm-start.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-comm_protocol=""
+comm_protocol="$1"
 
 start_iotdsm(){
 
-    if [ ! -f "build/libs/iotdsm-broker-1.0.0-all.jar" ]; then
+    if [ ! -f "build/libs/iotdsm-broker-1.0-SNAPSHOT-all.jar" ]; then
         bash -c "gradle build shadowJar"
     fi
 
     properties_file=""
     if [ "$comm_protocol" = "http" ]; then
-        properties_file="config.properties"
+        properties_file="http.properties"
     elif [ "$comm_protocol" = "mqtt" ]; then
-        properties_file="config.properties"
+        properties_file="mqtt.properties"
     elif [ "$comm_protocol" = "coap" ]; then
-        properties_file="config.properties"
+        properties_file="coap.properties"
     fi
     
     echo "init iotdsm-b application..."
-    bash -c "java -jar build/libs/iotdsm-broker-1.0.0-all.jar -c=$properties_file"
+    echo "java -jar build/libs/iotdsm-broker-1.0-SNAPSHOT-all.jar -c=$PWD/$properties_file"
+    java -jar build/libs/iotdsm-broker-1.0-SNAPSHOT-all.jar -c="$PWD/$properties_file"
 }
 
 start_iotdsm
