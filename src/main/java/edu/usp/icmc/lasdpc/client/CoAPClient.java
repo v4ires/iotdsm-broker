0c464386bb36261f9cdaf80ffab11fd18a3300cb
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-05-28 15:34:16 -0300

    update

--- src/main/java/edu/usp/icmc/lasdpc/client/CoAPClient.java
+++ src/main/java/edu/usp/icmc/lasdpc/client/CoAPClient.java
@@ -1,11 +1,18 @@
 package edu.usp.icmc.lasdpc.client;
 
+import edu.usp.icmc.lasdpc.util.PropertiesReader;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
 import org.eclipse.californium.core.CoapClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -15,13 +22,23 @@ import static org.eclipse.californium.core.coap.MediaTypeRegistry.APPLICATION_JS
 
 public class CoAPClient {
 
+    private static String _log4jFile = "log4j.properties";
+    private static String _logLevel = "ALL";
+    private static final Logger log = LoggerFactory.getLogger(CoAPClient.class);
+
+
     public static void run(String msg, int host) {
         String url [] = {"coap://localhost:5683/sensor", "coap://tpnode10:5683/sensor"};
         CoapClient coapClient = new CoapClient(url[host]);
-        coapClient.post(msg, APPLICATION_JSON);
+        if(coapClient.post(msg, APPLICATION_JSON).isSuccess()){
+            log.info("Foi");
+        }else {
+            log.info("Não Foi");
+        }
     }
 
     public static void main(String[] args) throws IOException {
+        enableLog4J(_logLevel);
         String msg = "{\"id\":\"1\",\"name\":\"dht11-0\",\"lat\":\"-22.0039007\",\"lng\":\"-47.891811\",\"name\":\"temperature sensor\",\"create_time\":\"2018-02-12 09:29:05.441\",\"description\":\"the DHT11 is a temperature and humidity sensor\",\"sensor_source\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"descrition\":\"sensor network of temperature and humidity\",\"name\":\"dht11-sensor\"},\"sensor_measure\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"value\":\"36.4\",\"sensor_measure_type\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"unit\":\"C\"}}}";
         int core = Runtime.getRuntime().availableProcessors();
         int host = Integer.parseInt(args[1]);
@@ -43,5 +60,14 @@ public class CoAPClient {
                 e.printStackTrace();
             }
         }, 1, 100L, TimeUnit.MILLISECONDS);
+        System.out.println("Terminou");
+    }
+
+    private static void enableLog4J(String logLevel) {
+        LogManager.getRootLogger().setLevel(Level.toLevel(_logLevel));
+        Properties properties = PropertiesReader.initialize(_log4jFile);
+        LogManager.getRootLogger().setLevel(Level.toLevel(logLevel));
+        LogManager.resetConfiguration();
+        PropertyConfigurator.configure(properties);
     }
 }
--- src/main/java/edu/usp/icmc/lasdpc/client/MQTTClient.java
+++ src/main/java/edu/usp/icmc/lasdpc/client/MQTTClient.java
@@ -1,16 +1,23 @@
 package edu.usp.icmc.lasdpc.client;
 
 
+import edu.usp.icmc.lasdpc.util.PropertiesReader;
 import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.mqtt.MqttClient;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -22,6 +29,11 @@ public class MQTTClient extends AbstractVerticle {
     private static final String MQTT_MESSAGE = "Alo 123 Testando";
     private static final String[] BROKER_HOST = {"localhost", "tpnode10"};
     private static final int BROKER_PORT = 1883;
+
+    private static String _log4jFile = "log4j.properties";
+    private static String _logLevel = "ALL";
+    private static final Logger log = LoggerFactory.getLogger(CoAPClient.class);
+
 
     public static void run(String msg, int host) {
         Vertx vertx = Vertx.vertx();
@@ -36,14 +48,15 @@ public class MQTTClient extends AbstractVerticle {
                         false,
                         s -> mqttClient.disconnect());
                 vertx.close();
+                log.info("Foi");
             } else {
-                System.out.println("Failed to connect to a server");
-                System.out.println(ch.cause());
+                log.info("Não Foi");
             }
         });
     }
 
     public static void main(String[] args) throws IOException {
+        enableLog4J(_logLevel);
         String msg = "{\"id\":\"1\",\"name\":\"dht11-0\",\"lat\":\"-22.0039007\",\"lng\":\"-47.891811\",\"name\":\"temperature sensor\",\"create_time\":\"2018-02-12 09:29:05.441\",\"description\":\"the DHT11 is a temperature and humidity sensor\",\"sensor_source\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"descrition\":\"sensor network of temperature and humidity\",\"name\":\"dht11-sensor\"},\"sensor_measure\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"value\":\"36.4\",\"sensor_measure_type\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"unit\":\"C\"}}}";
         int core = Runtime.getRuntime().availableProcessors();
         int host = Integer.parseInt(args[1]);
@@ -65,5 +78,14 @@ public class MQTTClient extends AbstractVerticle {
                 e.printStackTrace();
             }
         }, 1, 100L, TimeUnit.MILLISECONDS);
+        System.out.println("Terminou experimento");
+    }
+
+    private static void enableLog4J(String logLevel) {
+        LogManager.getRootLogger().setLevel(Level.toLevel(_logLevel));
+        Properties properties = PropertiesReader.initialize(_log4jFile);
+        LogManager.getRootLogger().setLevel(Level.toLevel(logLevel));
+        LogManager.resetConfiguration();
+        PropertyConfigurator.configure(properties);
     }
 }
