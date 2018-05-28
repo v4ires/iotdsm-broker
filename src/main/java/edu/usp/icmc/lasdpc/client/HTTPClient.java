bf18934152f7666406e68eed1b7a52ed2472048a
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-05-28 14:41:02 -0300

    Fixed Clients

--- src/main/java/edu/usp/icmc/lasdpc/client/HTTPClient.java
+++ src/main/java/edu/usp/icmc/lasdpc/client/HTTPClient.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class HTTPClient {
 
-    public static void run(String msg){
+    public static void run(String msg, int host){
         Vertx vertx = Vertx.vertx();
         WebClient client = WebClient.create(vertx);
 
@@ -35,15 +35,16 @@ public class HTTPClient {
     public static void main(String[] args) throws IOException {
         String msg = "{\"id\":\"1\",\"name\":\"dht11-0\",\"lat\":\"-22.0039007\",\"lng\":\"-47.891811\",\"name\":\"temperature sensor\",\"create_time\":\"2018-02-12 09:29:05.441\",\"description\":\"the DHT11 is a temperature and humidity sensor\",\"sensor_source\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"descrition\":\"sensor network of temperature and humidity\",\"name\":\"dht11-sensor\"},\"sensor_measure\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"value\":\"36.4\",\"sensor_measure_type\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"unit\":\"C\"}}}";
         int core = Runtime.getRuntime().availableProcessors();
+        int host = Integer.parseInt(args[1]);
         ScheduledExecutorService execService = Executors.newScheduledThreadPool(core);
         AtomicInteger atomicInteger = new AtomicInteger(0);
         BufferedWriter bw = new BufferedWriter(new FileWriter(new File("http.log")));
         execService.scheduleAtFixedRate(() -> {
             try {
                 Long t0 = System.nanoTime();
-                run(msg);
+                run(msg, host);
                 atomicInteger.incrementAndGet();
-                if (atomicInteger.get() == Integer.parseInt(args[0])) {
+                if (atomicInteger.get() > Integer.parseInt(args[0])) {
                     bw.close();
                     System.exit(0);
                 }
--- src/main/java/edu/usp/icmc/lasdpc/client/MQTTClient.java
+++ src/main/java/edu/usp/icmc/lasdpc/client/MQTTClient.java
@@ -23,7 +23,7 @@ public class MQTTClient extends AbstractVerticle {
     private static final String[] BROKER_HOST = {"localhost", "tpnode08"};
     private static final int BROKER_PORT = 1883;
 
-    public static void run(String msg) {
+    public static void run(String msg, int host) {
         Vertx vertx = Vertx.vertx();
         MqttClient mqttClient = MqttClient.create(vertx);
         mqttClient.connect(BROKER_PORT, BROKER_HOST[0], ch -> {
@@ -34,7 +34,8 @@ public class MQTTClient extends AbstractVerticle {
                         MqttQoS.AT_LEAST_ONCE,
                         false,
                         false,
-                        s -> vertx.close());
+                        s -> mqttClient.disconnect());
+                vertx.close();
             } else {
                 System.out.println("Failed to connect to a server");
                 System.out.println(ch.cause());
@@ -45,15 +46,16 @@ public class MQTTClient extends AbstractVerticle {
     public static void main(String[] args) throws IOException {
         String msg = "{\"id\":\"1\",\"name\":\"dht11-0\",\"lat\":\"-22.0039007\",\"lng\":\"-47.891811\",\"name\":\"temperature sensor\",\"create_time\":\"2018-02-12 09:29:05.441\",\"description\":\"the DHT11 is a temperature and humidity sensor\",\"sensor_source\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"descrition\":\"sensor network of temperature and humidity\",\"name\":\"dht11-sensor\"},\"sensor_measure\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"value\":\"36.4\",\"sensor_measure_type\":{\"create_time\":\"2018-02-12 09:29:05.441\",\"unit\":\"C\"}}}";
         int core = Runtime.getRuntime().availableProcessors();
+        int host = Integer.parseInt(args[1]);
         ScheduledExecutorService execService = Executors.newScheduledThreadPool(core);
         AtomicInteger atomicInteger = new AtomicInteger(0);
         BufferedWriter bw = new BufferedWriter(new FileWriter(new File("mqtt.log")));
         execService.scheduleAtFixedRate(() -> {
             try {
                 Long t0 = System.nanoTime();
-                run(msg);
+                run(msg, host);
                 atomicInteger.incrementAndGet();
-                if (atomicInteger.get() == 1000) {
+                if (atomicInteger.get() > Integer.parseInt(args[0])) {
                     bw.close();
                     System.exit(0);
                 }
