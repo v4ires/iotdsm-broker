1f6a6826bc336dd172a78d5d6867839c9beeb1d6
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-05-28 19:29:21 -0300

    update

--- run-mqtt.sh
+++ run-mqtt.sh
@@ -1,8 +1,8 @@
 echo "running 250 threads mqtt"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.MQTTClient 7500 1 mqtt1.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.MQTTClient 750 1 mqtt1.log
 
 echo "running 500 threads mqtt"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.MQTTClient 15000 1 mqtt2.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.MQTTClient 1500 1 mqtt2.log
 
 echo "running 1000 threads mqtt"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.MQTTClient 30000 1 mqtt3.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.MQTTClient 3000 1 mqtt3.log
