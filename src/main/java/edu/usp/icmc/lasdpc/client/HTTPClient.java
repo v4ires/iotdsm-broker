7e2d9ab0fb92785989a02d1adab18544a4a7c872
pinobex <sergiobaptistajr@gmail.com>
2018-05-28 12:12:25 -0300

    Create http client

--- src/main/java/edu/usp/icmc/lasdpc/client/HTTPClient.java
+++ src/main/java/edu/usp/icmc/lasdpc/client/HTTPClient.java
@@ -1,8 +1,28 @@
 package edu.usp.icmc.lasdpc.client;
 
+import edu.usp.icmc.lasdpc.util.PropertiesReader;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+
 public class HTTPClient {
 
     public static void main(String[] args) {
+
+        Vertx vertx = Vertx.vertx();
+        WebClient client = WebClient.create(vertx);
+
+        String service_hostname = "localhost";
+        int service_port = 8082;
+        String service_path = "/sensor";
+
+        client.post(service_port,  service_hostname, service_path).sendJson("lalala", ar ->{
+            if(ar.succeeded()){
+                System.out.println("Foi");
+            }else {
+                System.out.println("Não Foi");
+            }
+            vertx.close();
+        });
 
     }
 }
