3aec5bc7cd894d2766027e923c7b55cbb9ab2fd0
Vinícius Aires Barros <viniciusaires7@gmail.com>
2018-05-28 21:32:29 -0300

    update

--- run-coap.sh
+++ run-coap.sh
@@ -1,8 +1,8 @@
 echo "running 250 threads coap"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.CoAPClient 750 1 coap1.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.CoAPClient 300 1 coap1.log
 
 echo "running 500 threads coap"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.CoAPClient 1500 1 coap2.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.CoAPClient 600 1 coap2.log
 
 echo "running 1000 threads coap"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.CoAPClient 3000 1 coap3.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.CoAPClient 900 1 coap3.log
--- run-http.sh
+++ run-http.sh
@@ -1,8 +1,8 @@
 echo "running 250 threads http"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.HTTPClient 750 1 http1.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.HTTPClient 300 1 http1.log
 
 echo "running 500 threads http"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.HTTPClient 1500 1 http2.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.HTTPClient 600 1 http2.log
 
 echo "running 1000 threads http"
-java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.HTTPClient 3000 1 http3.log
+java -cp iotdsm-broker-1.0-SNAPSHOT-all.jar edu.usp.icmc.lasdpc.client.HTTPClient 900 1 http3.log
